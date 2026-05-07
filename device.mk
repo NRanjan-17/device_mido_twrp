00967566a446c59558c93efd9f7a791ddd85d59b
Nalinish Ranjan <nalinishranjan05@gmail.com>
2026-05-08 00:51:53 +0530

    mido: k4.19: Build some libs from source
    Add additional libraries and binaries for recovery support.

--- device.mk
+++ device.mk
@@ -46,6 +46,17 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/vibrator/system/,$(TARGET_COPY_OUT_RECOVERY)/root/system/)
 
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libxml2 \
+    libicuuc
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so
+
 # Keystore
 PRODUCT_PACKAGES += \
     android.system.keystore2
